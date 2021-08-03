868c7960d604994ceb26db139604c7d46ef039e3
Pablo Benitez <pablo.benitez@incluit.com>
2021-08-03 14:21:16 -0300

    tarea

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,34 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
+/**
+ * TAREA:
+ *  Modelo de usuario
+ *      - id
+ *      - firstName: string
+ *      - lastName: string
+ *      - email: string
+ *      - password: string
+ *      - roles: string
+ * 
+ *  1. Crear un modulo que encaupsule la logica
+ * para el manejo de usuarios en nuestra API REST
+ *  buscar un usuario.
+ *  buscar un usuario por email.
+ *  crear un usuario.
+ *  editar un usuario.
+ *  reemplazar un usuario.
+ *  eliminar un usuario.
+ *  
+ * 2. Crear un modulo global que administre
+ * las tareas y los usuarios.
+ *  Una tarea puede ser creada por un solo
+ * usuario autor.
+ *  Un usuario puede tener asignado una o más
+ * tareas.
+ *  Una tarea puede estar asignado a ninguno
+ * o muchos usuarios.
+ */
 @Module({
   imports: [TasksModule],
   controllers: [AppController],
